60930f501cc1286efe491fe6e1ea891853d6803e
IDragonfire <bastian616-git@yahoo.de>
2012-09-13 00:44:18 +0200

    rework SkillLifeTap ... use know getMaxHealth() and getMaxMana() (not 100)

--- dev/riffic33/heroes/skills/SkillLifeTap.java
+++ dev/riffic33/heroes/skills/SkillLifeTap.java
@@ -11,64 +11,86 @@ import com.herocraftonline.heroes.characters.skill.ActiveSkill;
 import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
 import com.herocraftonline.heroes.characters.skill.SkillType;
 import com.herocraftonline.heroes.util.Messaging;
-
+import com.herocraftonline.heroes.util.Setting;
+import com.herocraftonline.heroes.util.Util;
 
 public class SkillLifeTap extends ActiveSkill {
-	
+
     public SkillLifeTap(Heroes plugin) {
         super(plugin, "Lifetap");
         setUsage("/skill lifetap");
-        setArgumentRange(0, 0);
+        setDescription("Convert %s health to %s mana.");
         setIdentifiers("skill lifetap");
-        setTypes(SkillType.SILENCABLE);  
+        setTypes(SkillType.SILENCABLE);
     }
-    
+
     @Override
     public ConfigurationSection getDefaultConfig() {
         ConfigurationSection node = super.getDefaultConfig();
         node.set("HealthLossPercentage", 10);
         node.set("Manaregain", 10);
         node.set("Threshold", 20);
-        return  node;
+        return node;
     }
-    
+
     @Override
     public String getDescription(Hero hero) {
-    	int healthLoss = (int) SkillConfigManager.getSetting(hero.getHeroClass(), this, "HealthLossPercentage", 10);
-    	int manaGain = (int) SkillConfigManager.getSetting(hero.getHeroClass(), this, "Manaregain", 10);
-    	int thresHold = (int) SkillConfigManager.getSetting(hero.getHeroClass(), this, "Threshold", 20);
-    	
-    	String base = String.format("Convert %s health to %s mana.", healthLoss+"%", manaGain);
-    	
-        return thresHold <= 0 ? base : base.concat(" Not available with less than " +thresHold+ "% health left");
+        int healthLoss = SkillConfigManager.getSetting(hero.getHeroClass(),
+                this, "HealthLossPercentage", 10);
+        int manaGain = SkillConfigManager.getSetting(hero.getHeroClass(), this,
+                "Manaregain", 10);
+        StringBuffer sb = new StringBuffer(String.format(getDescription(),
+                healthLoss + "%", manaGain));
+        int thresHold = SkillConfigManager.getSetting(hero.getHeroClass(),
+                this, "Threshold", 20);
+        if (thresHold > 0) {
+            sb.append(" Not available with less than ");
+            sb.append(thresHold);
+            sb.append("% health left.");
+        }
+        double cdSec = SkillConfigManager.getUseSetting(hero, this,
+                Setting.COOLDOWN, 45000, false) / 1000.0D;
+        if (cdSec > 0.0D) {
+            sb.append(" CD:");
+            sb.append(Util.formatDouble(cdSec));
+            sb.append("s");
+        }
+        int mana = SkillConfigManager.getUseSetting(hero, this, Setting.MANA,
+                30, false);
+        if (mana > 0) {
+            sb.append(" M:");
+            sb.append(mana);
+        }
+        return sb.toString();
     }
-   
-    
-    
+
     @Override
     public SkillResult use(Hero hero, String[] args) {
-    	Player player = hero.getPlayer();
-    	
-    	int pctLoss = (int) SkillConfigManager.getSetting(hero.getHeroClass(), this, "HealthLossPercentage", 10);
-    	int manaGain = (int) SkillConfigManager.getSetting(hero.getHeroClass(), this, "Manaregain", 10);
-    	int thresHold = (int) SkillConfigManager.getSetting(hero.getHeroClass(), this, "Threshold", 20);
-    	
-    	double max = hero.getMaxHealth();
-        if(hero.getHealth() < max*((double)thresHold/100D) || hero.getMana() >= 100){
-        	if(hero.getMana() >= 100){
-        		Messaging.send(player, "Mana is already full");
-        	}else{
-        		Messaging.send(player, "Health is too low to use life tap");
-        	}	
-        	return SkillResult.CANCELLED;
-        }else{
-        	int damage = (int) (Math.floor(max*((double)pctLoss/100D)));
-        	damageEntity(player, player, damage, DamageCause.CUSTOM);
-        	hero.setMana( hero.getMana() + manaGain > 100 ? 100 : hero.getMana()+manaGain ); 
-        	broadcast( player.getLocation() , "$1 used lifetap", player.getDisplayName() );
-        	return SkillResult.NORMAL;
+        Player player = hero.getPlayer();
+
+        int pctLoss = SkillConfigManager.getSetting(hero.getHeroClass(), this,
+                "HealthLossPercentage", 10);
+        int manaGain = SkillConfigManager.getSetting(hero.getHeroClass(), this,
+                "Manaregain", 10);
+        int thresHold = SkillConfigManager.getSetting(hero.getHeroClass(),
+                this, "Threshold", 20);
+
+        if (hero.getHealth() < hero.getMaxHealth() * (thresHold / 100D)) {
+            Messaging.send(player, "Health is too low to use life tap");
+            return SkillResult.CANCELLED;
         }
+        if (hero.getMana() >= hero.getMaxMana()) {
+            Messaging.send(player, "Mana is already full");
+            return SkillResult.CANCELLED;
+        }
+
+        int damage = (int) (Math.floor(hero.getMaxHealth() * (pctLoss / 100D)));
+        damageEntity(player, player, damage, DamageCause.CUSTOM);
+        int newMana = hero.getMana() + manaGain > hero.getMaxMana() ? hero
+                .getMaxMana() : hero.getMana() + manaGain;
+        hero.setMana(newMana);
+        Messaging.send(player, "You lose $1 health. New mana: $2",
+                new Object[] { damage + "", newMana + "" });
+        return SkillResult.NORMAL;
     }
-    
-    
 }
